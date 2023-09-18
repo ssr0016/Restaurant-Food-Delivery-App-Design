b101adf38906634c62da172b9c47fb2238b49314
ssr0016 <samsonrecluta0016@gmail.com>
2023-09-18 12:01:22 +0800

    adding some features in my project

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,8 +1,25 @@
+import { singleProduct } from '@/data'
+import Image from 'next/image'
 import React from 'react'
 
 const SingleProduct = () => {
   return (
-    <div>SingleProduct</div>
+    <div className='p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row'>
+      {/* IMAGE CONTAINER */}
+      {singleProduct.img && ( <div className='relative'>
+        <Image 
+        src={singleProduct.img}
+        alt=""
+        className='object-contain'
+        fill/>
+      </div>
+      )}
+      {/* TEXT CONTAINER */}
+      <div className=''>
+        <h1>{singleProduct.title}</h1>
+        <p>{singleProduct.desc}</p>
+      </div>
+    </div>
   )
 }
 
