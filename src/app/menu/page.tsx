56a12987cf9c1ab4c89c5961a9002972827feef1
ssr0016 <samsonrecluta0016@gmail.com>
2023-09-22 09:34:57 +0800

    fixing some bugs

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const MenuPage = () => {
   return (
-    <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
+    <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh -6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
       {menu.map((category) => (
         <Link
           href={`/menu/${category.slug}`}
@@ -12,10 +12,10 @@ const MenuPage = () => {
           className="w-full h-1/3 bg-cover p-8 md:h-1/2"
           style={{ backgroundImage: `url(${category.img})` }}
         >
-          <div className={`text-${category.color} w-1/2`}>
+          <div className={`text-${category.color} w-1/2`} >
             <h1 className="uppercase font-bold text-3xl">{category.title}</h1>
             <p className="text-sm my-8">{category.desc}</p>
-            <button className={`hidden 2xl:block bg-${category.color} text-${category.color === "black" ? "white" : "red-500"} py-2 px-4 rounded-md`}>Explore</button>
+            <button className={`hidden 2xl:block bg-${category.color} text-${category.color === "black" ? "white" : "red-500"} py-2 px-4 rounded-md` }>Explore</button>
           </div>
         </Link>
       ))}
--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -18,7 +18,7 @@ const SingleProductPage = () => {
         </div>
       )}
       {/* TEXT CONTAINER */}
-      <div className="h-1/2 flex flex-col gap-4  md:h-[70%] md:justify-center md:gap-6 xl:gap-8">
+      <div className="h-full flex flex-col gap-4 md:h-[70%] md:justify-center md:gap-6 xl:gap-8">
         <h1 className="text-3xl font-bold uppercase xl:text-5xl">{singleProduct.title}</h1>
         <p>{singleProduct.desc}</p>
         <Price price={singleProduct.price} id={singleProduct.id} options={singleProduct.options}/>
